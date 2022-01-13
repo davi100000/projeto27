b1c597a2fae087f67e2974118f0bd17116cb5a75
davi100000 <96146053+davi100000@users.noreply.github.com>
2022-01-13 18:02:47 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function setup() {
 function keyPressed() {
  if (keyCode === UP_ARROW) {
 
- 	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,
+ 	Matter.Body.applyForce(bob1.body,bob1.body.position,
                           {x:-50,y:-45});	 
    }
 }
